35650ad730cb274d64e8089e22f0f0bf912cf0d8
Vlad <v.cornici@gmail.com>
2023-01-22 12:22:20 +0200

    adding scanner program

--- TestingProgram/TestingProgram/src/TestingProgramPackage/Main.java
+++ TestingProgram/TestingProgram/src/TestingProgramPackage/Main.java
@@ -2,20 +2,15 @@ package TestingProgramPackage;
 
 import static TestingProgramPackage.NotMain.notInMain;
 
+
 public class Main {
-//    static int decreseFromMyNumber(int number) {
-//        System.out.println("Current number value is: " + number);
-//        if (number == 0) return 0;
-//
-//        number--;
-//
-//        return (decreseFromMyNumber(number));
-//    }
+
 
     public static void main(String[] args) {
         System.out.println("Main method:");
 
-        notInMain();
+        int myInt = Integer.parseInt("12341");
+        System.out.println(((Object) myInt).getClass().getSimpleName());
 
     }
 }
