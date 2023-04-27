324c5710536fa8ea4a8b33e82f92fb241a251eb8
ccf6 <ccf6@email.com>
2023-04-27 15:09:54 +0300

    new chnages for write and read from a file

--- ToDoList/ToDoList/src/main/java/com/example/todolist/HelloController.java
+++ ToDoList/ToDoList/src/main/java/com/example/todolist/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.todolist;
 
 import com.example.todolist.dataModel.ToDoItem;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class HelloController {
@@ -17,6 +22,8 @@ public class HelloController {
     private ListView<ToDoItem> todoListView;
     @FXML
     private TextArea itemDetailsTextArea;
+    @FXML
+    private Label dueData;
 
     public void initialize() {
         ToDoItem item1 = new ToDoItem("Buy some stuff", "Go to store and buy it.",
@@ -36,24 +43,38 @@ public class HelloController {
         toDoItems.add(item2);
         toDoItems.add(item3);
         toDoItems.add(item4);
+
+        todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<ToDoItem>() {
+            @Override
+            public void changed(ObservableValue<? extends ToDoItem> observableValue, ToDoItem oldItem, ToDoItem newItem) {
+                if (newItem != null) {
+                    ToDoItem selectedItem = todoListView.getSelectionModel().getSelectedItem();
+                    itemDetailsTextArea.setText(selectedItem.getDetails().toString());
+                    DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+                    dueData.setText(dateTimeFormatter.format(selectedItem.getDeadLine()));
+                }
+            }
+        });
         todoListView.getItems().setAll(toDoItems);
+        System.out.println(toDoItems);
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-
-    }
-
-    @FXML
-    public void handleClickListView() {
-        ToDoItem selectedItem = todoListView.getSelectionModel().getSelectedItem();
-        StringBuilder sb = new StringBuilder(selectedItem.getDetails());
-        itemDetailsTextArea.setText(selectedItem.getDetails());
-
-        sb.append("\n\n\n\n");
-        sb.append("Due: ");
-        sb.append(selectedItem.getDeadLine().toString());
-        itemDetailsTextArea.setText(sb.toString());
+        todoListView.getSelectionModel().selectFirst();
 
 
     }
+
+    public void stop() {
+    }
+
+//
+//    @FXML
+//    public void handleClickListView() {
+//        ToDoItem selectedItem = todoListView.getSelectionModel().getSelectedItem();
+//        itemDetailsTextArea.setText(selectedItem.getDetails().toString());
+//        dueData.setText(selectedItem.getDeadLine().toString());
+//
+//
+//    }
 
 
 }
